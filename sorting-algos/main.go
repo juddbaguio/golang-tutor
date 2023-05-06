364d702e1ff8f7e8d9070c955965f33343ebc4d9
Judd Misael R. Baguio <juddmisaelbaguio@gmail.com>
2023-05-06 08:54:24 +0800

    changes

--- sorting-algos/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "log"
-
-func main() {
-	var intSlice []int = []int{6, 3, 2, 4, 8, 9, 0, 1}
-
-	log.Println(QuickSort(intSlice))
-}
--- sorting-algos/merge_sort.go
+++
@@ -1,5 +0,0 @@
-package main
-
-func MergeSort(arr []int, p, q int) []int {
-	return arr
-}
