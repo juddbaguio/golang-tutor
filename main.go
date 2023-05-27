7385ac1500934471bd3c94713174e16dadaedcca
Judd Misael R. Baguio <juddmisaelbaguio@gmail.com>
2023-05-27 10:03:48 +0800

    added file upload

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -123,15 +125,59 @@ func (sh *SampleHandler) WithQueryParameters(w http.ResponseWriter, r *http.Requ
 	w.Write(nil) // direct to the point, sending it back to the client
 }
 
+func (sh *SampleHandler) WithFileUpload(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("File Upload Endpoint Hit")
+
+	// Parse our multipart form, 10 << 20 specifies a maximum
+	// upload of 10 MB files.
+	r.ParseMultipartForm(10 << 20)
+	// FormFile returns the first file for the given key `myFile`
+	// it also returns the FileHeader so we can get the Filename,
+	// the Header and the size of the file
+	file, handler, err := r.FormFile("myFile") // file <--- [][][][] <----- [][][][]
+	if err != nil {
+		fmt.Println("Error Retrieving the File")
+		fmt.Println(err)
+		return
+	}
+	defer file.Close() // closes reader connection, expects no data to be received from the client
+	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+	fmt.Printf("File Size: %+v\n", handler.Size)
+	fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+	// Create a temporary file within our temp-images directory that follows
+	// a particular naming pattern
+	tempFile, err := os.Create("./temp-images/" + handler.Filename)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer tempFile.Close()
+
+	// read all of the contents of our uploaded file into a
+	// byte array
+	// fileByte -> actual content
+	fileBytes, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// write this byte array to our temporary file
+	tempFile.Write(fileBytes)
+	// return that we have successfully uploaded our file!
+	fmt.Fprintf(w, "Successfully Uploaded File\n")
+}
+
 func main() {
 	var sampleHandler *SampleHandler = &SampleHandler{}
+
+	// creates temporary-directory
+	os.Mkdir("./temp-images", os.ModeAppend.Perm())
 
 	router := chi.NewRouter()
 
 	// nouns/adjective
 	// "/set-active-user" (very wrong)
 	// "/delete-user" (very wrong)
-	// [PUT] "/users/status"
+	// [PUT/PATCH] "/users/status"
 	// [DELETE] "/users"
 	// [DELETE] "/users/{userId}"
 
@@ -141,7 +187,12 @@ func main() {
 	router.Get("/content-type", sampleHandler.WithContentType)
 	router.Get("/query-params", sampleHandler.WithQueryParameters)
 	router.Get("/json", sampleHandler.WithJSONResponse)
+	router.Post("/file-upload", sampleHandler.WithFileUpload)
+	router.Put("/file-upload", sampleHandler.WithFileUpload)
 	router.Post("/json-body", sampleHandler.WithJSONBodyPayload) // or PUT, DELETE, PATCH
+	router.Put("/json-body", sampleHandler.WithJSONBodyPayload)
+	router.Delete("/json-body", sampleHandler.WithJSONBodyPayload)
+	router.Patch("/json-body", sampleHandler.WithJSONBodyPayload)
 
 	if err := http.ListenAndServe("127.0.0.1:8080", router); err != nil {
 		log.Println(err)
